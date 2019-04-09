bb613b4870bf011aa9aa3b51717d1669522de845
Zaharenko <anton.zaharenko@gmail.com>
2019-04-10 02:35:26 +0300

    desctop version

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,38 @@
 $(document).ready(function() {
+
+    (function() {
+
+        'use strict';
+
+        var $searchView = $('.menu-search-container');
+        var $menu = $('.nav-item, .menu-search');
+        var $fadeScreen = $('.fade-screen');
+
+        $('li.menu-search a, .fade-screen, .menu-search-close').on('click', function(e) {
+
+            $searchView.toggleClass('active');
+
+            setTimeout(function() {
+                $searchView.children().find('input').focus();
+            }, 1100);
+
+            $fadeScreen.toggleClass('visible');
+            $menu.removeClass('is-closed');
+            $menu.toggleClass('hidden');
+
+            e.preventDefault();
+        });
+
+        $('.fade-screen,.menu-search-close').on('click', function(e) {
+            $menu.toggleClass('is-closed');
+            e.preventDefault();
+        });
+
+    }())
+
+
+
+
     $('.remove-before-release').click(function() {
         $(".remove-before-release-list").toggleClass('release-list');
     });
